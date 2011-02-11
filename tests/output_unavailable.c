97ac39baf2f3c43c3b7caecbcfad96d26294dc68
Justin R. Wilson <jrwilson@go.wustl.edu>
2011-02-11 15:23:39 -0600

    Added OUTPUT_UNAVAILABLE test.

--- tests/output_unavailable.c
+++ tests/output_unavailable.c
@@ -50,7 +50,13 @@ child_system_output (void* state, void* param)
 }
 
 static void
-child_input (void* state, void* param, bid_t bid)
+child_input1 (void* state, void* param, bid_t bid)
+{
+
+}
+
+static void
+child_input2 (void* state, void* param, bid_t bid)
 {
 
 }
@@ -61,7 +67,7 @@ child_output (void* state, void* param)
   return -1;
 }
 
-static input_t child_inputs[] = { child_input, NULL };
+static input_t child_inputs[] = { child_input1, child_input2, NULL };
 static output_t child_outputs[] = { child_output, NULL };
 static internal_t child_internals[] = { NULL };
 
@@ -81,40 +87,42 @@ typedef enum {
   CREATE1_SENT,
   CREATE2_UNSENT,
   CREATE2_SENT,
-  COMPOSE_UNSENT,
-  COMPOSE_SENT,
-} composed_state_t;
+  COMPOSE1_UNSENT,
+  COMPOSE1_SENT,
+  COMPOSE2_UNSENT,
+  COMPOSE2_SENT,
+} output_unavailable_state_t;
 
 typedef struct {
-  composed_state_t state;
+  output_unavailable_state_t state;
   aid_t child1;
   aid_t child2;
-} composed_t;
+} output_unavailable_t;
 
 static void*
-composed_create (void)
+output_unavailable_create (void)
 {
-  composed_t* composed = malloc (sizeof (composed_t));
-  composed->state = START;
+  output_unavailable_t* output_unavailable = malloc (sizeof (output_unavailable_t));
+  output_unavailable->state = START;
 
-  return composed;
+  return output_unavailable;
 }
 
 static void
-composed_system_input (void* state, void* param, bid_t bid)
+output_unavailable_system_input (void* state, void* param, bid_t bid)
 {
-  composed_t* composed = state;
-  assert (composed != NULL);
+  output_unavailable_t* output_unavailable = state;
+  assert (output_unavailable != NULL);
 
   assert (bid != -1);
   assert (buffer_size (bid) == sizeof (receipt_t));
   const receipt_t* receipt = buffer_read_ptr (bid);
 
-  switch (composed->state) {
+  switch (output_unavailable->state) {
   case START:
     switch (receipt->type) {
     case SELF_CREATED:
-      composed->state = CREATE1_UNSENT;
+      output_unavailable->state = CREATE1_UNSENT;
       schedule_system_output ();
       break;
     case BAD_ORDER:
@@ -146,8 +154,8 @@ composed_system_input (void* state, void* param, bid_t bid)
   case CREATE1_SENT:
     switch (receipt->type) {
     case CHILD_CREATED:
-      composed->state = CREATE2_UNSENT;
-      composed->child1 = receipt->child_created.child;
+      output_unavailable->state = CREATE2_UNSENT;
+      output_unavailable->child1 = receipt->child_created.child;
       schedule_system_output ();
       break;
     case BAD_ORDER:
@@ -179,8 +187,8 @@ composed_system_input (void* state, void* param, bid_t bid)
   case CREATE2_SENT:
     switch (receipt->type) {
     case CHILD_CREATED:
-      composed->state = COMPOSE_UNSENT;
-      composed->child2 = receipt->child_created.child;
+      output_unavailable->state = COMPOSE1_UNSENT;
+      output_unavailable->child2 = receipt->child_created.child;
       schedule_system_output ();
       break;
     case BAD_ORDER:
@@ -206,13 +214,15 @@ composed_system_input (void* state, void* param, bid_t bid)
       assert (0);
     }
     break;
-  case COMPOSE_UNSENT:
+  case COMPOSE1_UNSENT:
     assert (0);
     break;
-  case COMPOSE_SENT:
+  case COMPOSE1_SENT:
     switch (receipt->type) {
     case COMPOSED:
-      exit (EXIT_SUCCESS);
+      output_unavailable->state = COMPOSE2_UNSENT;
+      schedule_system_output ();
+      break;
     case CHILD_CREATED:
     case BAD_ORDER:
     case SELF_CREATED:
@@ -222,6 +232,37 @@ composed_system_input (void* state, void* param, bid_t bid)
     case INPUT_DNE:
     case OUTPUT_UNAVAILABLE:
     case INPUT_UNAVAILABLE:
+    case INPUT_COMPOSED:
+    case OUTPUT_COMPOSED:
+    case NOT_COMPOSER:
+    case NOT_COMPOSED:
+    case DECOMPOSED:
+    case INPUT_DECOMPOSED:
+    case OUTPUT_DECOMPOSED:
+    case RESCINDED:
+    case AUTOMATON_DNE:
+    case NOT_OWNER:
+    case CHILD_DESTROYED:
+      assert (0);
+    }
+    break;
+  case COMPOSE2_UNSENT:
+    assert (0);
+    break;
+  case COMPOSE2_SENT:
+    switch (receipt->type) {
+    case OUTPUT_UNAVAILABLE:
+      exit (EXIT_SUCCESS);
+      break;
+    case BAD_ORDER:
+    case SELF_CREATED:
+    case BAD_DESCRIPTOR:
+    case CHILD_CREATED:
+    case DECLARED:
+    case OUTPUT_DNE:
+    case INPUT_DNE:
+    case INPUT_UNAVAILABLE:
+    case COMPOSED:
     case INPUT_COMPOSED:
     case OUTPUT_COMPOSED:
     case NOT_COMPOSER:
@@ -240,22 +281,22 @@ composed_system_input (void* state, void* param, bid_t bid)
 }
 
 static bid_t
-composed_system_output (void* state, void* param)
+output_unavailable_system_output (void* state, void* param)
 {
-  composed_t* composed = state;
-  assert (composed != NULL);
+  output_unavailable_t* output_unavailable = state;
+  assert (output_unavailable != NULL);
 
   bid_t bid = buffer_alloc (sizeof (order_t));
   order_t* order = buffer_write_ptr (bid);
 
-  switch (composed->state) {
+  switch (output_unavailable->state) {
   case START:
     assert (0);
     break;
   case CREATE1_UNSENT:
     /* Send a create order. */
     order_create_init (order, &child_descriptor);
-    composed->state = CREATE1_SENT;
+    output_unavailable->state = CREATE1_SENT;
     break;
   case CREATE1_SENT:
     assert (0);
@@ -263,17 +304,25 @@ composed_system_output (void* state, void* param)
   case CREATE2_UNSENT:
     /* Send a create order. */
     order_create_init (order, &child_descriptor);
-    composed->state = CREATE2_SENT;
+    output_unavailable->state = CREATE2_SENT;
     break;
   case CREATE2_SENT:
     assert (0);
     break;
-  case COMPOSE_UNSENT:
+  case COMPOSE1_UNSENT:
     /* Send a compose order. */
-    order_compose_init (order, composed->child1, child_output, NULL, composed->child2, child_input, NULL);
-    composed->state = COMPOSE_SENT;
+    order_compose_init (order, output_unavailable->child1, child_output, NULL, output_unavailable->child2, child_input1, NULL);
+    output_unavailable->state = COMPOSE1_SENT;
     break;
-  case COMPOSE_SENT:
+  case COMPOSE1_SENT:
+    assert (0);
+    break;
+  case COMPOSE2_UNSENT:
+    /* Send a compose order. */
+    order_compose_init (order, output_unavailable->child1, child_output, NULL, output_unavailable->child2, child_input2, NULL);
+    output_unavailable->state = COMPOSE2_SENT;
+    break;
+  case COMPOSE2_SENT:
     assert (0);
     break;
   }
@@ -281,22 +330,22 @@ composed_system_output (void* state, void* param)
   return bid;
 }
 
-static input_t composed_inputs[] = { NULL };
-static output_t composed_outputs[] = { NULL };
-static internal_t composed_internals[] = { NULL };
+static input_t output_unavailable_inputs[] = { NULL };
+static output_t output_unavailable_outputs[] = { NULL };
+static internal_t output_unavailable_internals[] = { NULL };
 
-descriptor_t composed_descriptor = {
-  .constructor = composed_create,
-  .system_input = composed_system_input,
-  .system_output = composed_system_output,
-  .inputs = composed_inputs,
-  .outputs = composed_outputs,
-  .internals = composed_internals,
+descriptor_t output_unavailable_descriptor = {
+  .constructor = output_unavailable_create,
+  .system_input = output_unavailable_system_input,
+  .system_output = output_unavailable_system_output,
+  .inputs = output_unavailable_inputs,
+  .outputs = output_unavailable_outputs,
+  .internals = output_unavailable_internals,
 };
 
 int
 main (int argc, char** argv)
 {
-  ueioa_run (&composed_descriptor);
+  ueioa_run (&output_unavailable_descriptor);
   exit (EXIT_SUCCESS);
 }
