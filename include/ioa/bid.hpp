674185ea34fe8a26d2331c9ca032e10a320e03d0
Justin Wilson <jrwilson@go.wustl.edu>
2011-06-04 12:37:00 -0500

    Changed bid to machine width.

--- include/ioa/bid.hpp
+++ include/ioa/bid.hpp
@@ -1,6 +1,14 @@
 #ifndef __bid_hpp__
 #define __bid_hpp__
 
+#include <ioa/environment.hpp>
+
+#include <stdint.h>
+
+#include <ioa/environment.hpp>
+
+#include <stdint.h>
+
 namespace ioa {
 
   /*
@@ -8,7 +16,12 @@ namespace ioa {
 
     I found that associating a unique ID with each successful bind is good because the user doesn't have to re-supply the bind parameters when unbinding and the only possible error is that the binding doesn't exist.
    */
-  typedef int bid_t;
+#ifdef ENVIRONMENT64
+  typedef int64_t bid_t;
+#endif
+#ifdef ENVIRONMENT32
+  typedef int32_t bid_t;
+#endif
 
 }
 
