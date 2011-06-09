38369ff9ffd2fcb8007c77e905c37dc227956b0b
Justin R. Wilson <jrwilson@go.wustl.edu>
2011-06-08 20:56:12 -0500

    Fixed macros in automaton1.

--- test/automaton2.hpp
+++ test/automaton2.hpp
@@ -10,37 +10,45 @@ private:
   float m_pole;
   int m_vole;
 
-  UV_UP_INPUT (automaton2, uv_up_input) { }
-  UV_P_INPUT (automaton2, uv_p_input, float, pole) { m_pole += pole; }
-  V_UP_INPUT (automaton2, v_up_input, int, vole) { m_vole += vole; }
-  V_P_INPUT (automaton2, v_p_input, int, vole, float, pole) { m_vole += vole; m_pole += pole; }
+  DECLARE_UV_UP_INPUT (automaton2, uv_up_input);
+  DECLARE_UV_P_INPUT (automaton2, uv_p_input, float, pole);
+  DECLARE_V_UP_INPUT (automaton2, v_up_input, int, vole);
+  DECLARE_V_P_INPUT (automaton2, v_p_input, int, vole, float, pole);
 
-  UV_UP_OUTPUT (automaton2, uv_up_output) { return false; }
-  UV_P_OUTPUT (automaton2, uv_p_output, float, pole) { m_pole += pole; return false; }
-  V_UP_OUTPUT (automaton2, v_up_output, int) { return std::pair<bool, int> (); }
-  V_P_OUTPUT (automaton2, v_p_output, int, float, pole) { m_pole += pole; return std::pair<bool, int> (); }
+  DECLARE_UV_UP_OUTPUT (automaton2, uv_up_output);
+  DECLARE_UV_P_OUTPUT (automaton2, uv_p_output, float, pole);
+  DECLARE_V_UP_OUTPUT (automaton2, v_up_output, int);
+  DECLARE_V_P_OUTPUT (automaton2, v_p_output, int, float, pole);
 
   DECLARE_UP_INTERNAL (automaton2, up_internal);
-
-  P_INTERNAL (automaton2, p_internal, float, pole) { m_pole += pole; }
+  DECLARE_P_INTERNAL (automaton2, p_internal, float, pole);
   
-  UV_UP_INPUT (automaton2, uv_up_input2) { }
+  DECLARE_UV_UP_INPUT (automaton2, uv_up_input2);
+
 public:
   automaton2 () :
-    ACTION (automaton2, uv_up_input),
-    ACTION (automaton2, uv_p_input),
-    ACTION (automaton2, v_up_input),
-    ACTION (automaton2, v_p_input),
     ACTION (automaton2, uv_up_output),
     ACTION (automaton2, uv_p_output),
     ACTION (automaton2, v_up_output),
-    ACTION (automaton2, v_p_output),
-    ACTION (automaton2, p_internal),
-    ACTION (automaton2, uv_up_input2)
+    ACTION (automaton2, v_p_output)
   { }
 
 };
 
+DEFINE_UV_UP_INPUT (automaton2, uv_up_input) { }
+DEFINE_UV_P_INPUT (automaton2, uv_p_input, float, pole) { m_pole += pole; }
+DEFINE_V_UP_INPUT (automaton2, v_up_input, int, vole) { m_vole += vole; }
+DEFINE_V_P_INPUT (automaton2, v_p_input, int, vole, float, pole) { m_vole += vole; m_pole += pole; }
+
+DEFINE_UV_UP_OUTPUT (automaton2, uv_up_output) { return false; }
+DEFINE_UV_P_OUTPUT (automaton2, uv_p_output, float, pole) { m_pole += pole; return false; }
+DEFINE_V_UP_OUTPUT (automaton2, v_up_output, int) { return std::pair<bool, int> (); }
+DEFINE_V_P_OUTPUT (automaton2, v_p_output, int, float, pole) { m_pole += pole; return std::pair<bool, int> (); }
+
+
 DEFINE_UP_INTERNAL (automaton2, up_internal) { }
+DEFINE_P_INTERNAL (automaton2, p_internal, float, pole) { m_pole += pole; }
+
+DEFINE_UV_UP_INPUT (automaton2, uv_up_input2) { }
 
 #endif
