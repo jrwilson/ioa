49f189570644c44894b4250271df9efdb6d1b917
Justin R. Wilson <jrwilson@go.wustl.edu>
2011-06-20 11:02:45 -0500

    First draft of null_automaton tutorial.

---
+++ tutorial/null_automaton.cpp
@@ -0,0 +1,12 @@
+#include <ioa/ioa.hpp>
+#include <ioa/global_fifo_scheduler.hpp>
+
+class null_automaton :
+  public ioa::automaton
+{ };
+
+int main () {
+  ioa::global_fifo_scheduler sched;
+  ioa::run (sched, ioa::make_generator<null_automaton> ());
+  return 0;
+}
